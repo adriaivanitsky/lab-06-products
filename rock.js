d3a92637be038d94ffc96e3e36f96cbe771b4c13
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-11 16:16:12 -0700

    created an array of 5 products, made it a variable, and exported it

--- rock.js
+++ rock.js
@@ -1,9 +1,33 @@
 export const rocks = [
     {
-        name = chrysoprase
-        id = chrysoprase
-        img = 
-        price =
+        id: '1',
+        name: 'chrysoprase',
+        img: 'assets/chrysoprase.jpeg',
+        price: '5',
 
-    };
-]
+    },
+    {
+        id: '2',
+        name: 'azurite',
+        img: 'assets/azurite.jpeg',
+        price: '40',
+    },
+    {
+        id: '3',
+        name: 'pyrite',
+        img: 'assets/pyrite.jpeg',
+        price: '60',
+    },
+    {
+        id: '4',
+        name: 'labradorite',
+        img: 'assets/labradorite.png',
+        price: '50',
+    },
+    {
+        id: '5',
+        name: 'obsidian',
+        img: 'assets/obsidian.png',
+        price: '80',
+    }
+];
