622809f274555dd7f6276bbb64f4f9370941be04
Adria Ivanitsky <littleivanitsky@gmail.com>
2021-10-13 10:22:03 -0700

    added function to calculate order total

--- utils.js
+++ utils.js
@@ -5,3 +5,15 @@ export function findById(id, items){
         } 
     }
 }
+
+export function calcOrderTotal(cart, rocks){
+    let orderTotal = 0;
+    for (let item of cart){
+        const rock = findById(item.id, rocks);
+        orderTotal = orderTotal + rock.price * item.qty;
+    }
+    return orderTotal;
+}
+
+export function toUSD(number){
+    return number.toLocaleString('en-us', { style: 'currency', currency: 'USD' });
